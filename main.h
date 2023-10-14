7cd7c88d84460809d30caeb202426bc046721829
Mohd-abdallah <dawry798@gmail.com>
2023-10-15 00:59:02 +0200

    Creating README.md File and main.h file

---
+++ main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Standard Libraries */
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+
+
+/* Prototypes */
+
+
+
+
+
+/* Macros */
+
+
+
+
+/* Structures */
+
+
+
+#endif
