7bf278592c5274cda4ee25de3e22cdcecb354c3b
Mohd-abdallah <dawry798@gmail.com>
2023-10-17 19:35:52 +0200

    Fixing conflict & updating files

--- handle-s.c
+++ handle-s.c
@@ -1,23 +1,28 @@
 #include "main.h"
 /**
  * handle_s - a function that handles the conversion specifier %s.
- * @args: list of arguments.
+ * @args: list of argument.
  * Return: number of characters Printed.
  */
 int handle_s(va_list args)
 {
-	int char_count = 0;
+	int char_count, i = 0;
 	char *string;
 
 	string = va_arg(args, char *);
-	if (!string)
+	if (string == NULL)
 	{
-		return(-1);
+		return (-1);
 	}
-	else
+	else 
 	{
-		write(1, string, strlen(string));
-		char_count += strlen(string);
+		char_count = strlen(string);
+		while (i < char_count)
+		{
+			_putchar(string[i]);
+			i++;
+		}
+		return (char_count);
 	}
-	return(char_count);
+
 }
