b3958b8f6f9bcf2824b3db5391a5eab586b7dfa1
assma-codes <assma.hm89@gmail.com>
2023-10-17 19:01:06 -0700

    betty fix

--- _printf.c
+++ _printf.c
@@ -40,8 +40,6 @@ int _printf(const char *format, ...)
 				handle_d(args);
 			else if (*format == 'i')
 				handle_i(args);
-			else if (*format == 'o')
-				handle_oct(args);
 		}
 		format++;
 	}
